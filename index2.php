13a11791e003da9fff10e555849c0bef9c905d0c
miguelfrancaa <miguelcrfranca@gmail.com>
2023-05-12 14:56:53 +0100

    HTML TO PHP-JS HOVER ARTISTS feito-COM CSS

--- index2.php
+++ index2.php
@@ -2,7 +2,7 @@
 	require("includes/config.php");
 
 	$query = $db->prepare("
-		SELECT name, description_photo
+		SELECT name, photo
 		FROM artists
 		ORDER BY name
 		");
@@ -24,6 +24,26 @@
 		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<style type="text/css">
+			<?php
+				foreach ($artists as $artist) {
+					echo ".".strtolower(trim(str_replace(" ", "", $artist["name"])))."fundo{
+							position: fixed;
+							width: 100%;
+							height: 100vh;
+							z-index: -3;
+							background-image: url('img/artists/".$artist["photo"]."');
+							 -webkit-background-size: cover;
+						  	-moz-background-size: cover;
+						  	-o-background-size: cover;
+						  	background-size: cover;
+						  	background-position: center;
+						  animation-name: artists;
+						  animation-duration: 0.5s;
+							}";
+				}
+			?>
+		</style>
 	</head>
 <?php
 	require("includes/header.php");
@@ -31,7 +51,7 @@
 	<main>
 		<ul class="artistsList">
 <?php foreach ($artists as $artist) {
-				echo'<li class="artists '.strtolower(trim(str_replace(" ", "", $artist["name"]))).'"><a href="">'.$artist["name"].'</a></li><br><br><br><br><br><br>';
+				echo'<li class="artists fundo '.strtolower(trim(str_replace(" ", "", $artist["name"]))).'"><a href="">'.$artist["name"].'</a></li><br><br><br><br><br><br>';
 			};
 ?>
 		</ul>
@@ -41,5 +61,20 @@
 	<script src="js/jquery-3.6.0.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/script.js"></script>
+	<script type="text/javascript">
+<?php
+	foreach ($artists as $artist) {
+		echo '
+		$(".'.strtolower(trim(str_replace(" ", "", $artist["name"]))).'").hover(
+		  function () {
+		    $(".fundartist").addClass("'.strtolower(trim(str_replace(" ", "", $artist["name"]))).'fundo");
+		  }, 
+		  function () {
+		    $(".fundartist").removeClass("'.strtolower(trim(str_replace(" ", "", $artist["name"]))).'fundo");
+		  }
+  		  );';
+	}
+?>
+	</script>
 	</body>
 </html>
