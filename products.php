1798002fdc03ceaad239d8c78c7136316e2e37c7
miguelfrancaa <miguelcrfranca@gmail.com>
2023-05-16 22:52:02 +0100

    PRODUCTS feito, resolver pequeno problema na responsividade

--- products.php
+++ products.php
@@ -49,12 +49,12 @@
 <?php
 	foreach ($products as $product) {
 		echo "<a href='productdetails.php?product_id=".$product["product_id"]."'><div class='row rela'>
-				<div class='col-md-7 col-sm-12 col-xs-12'><img class='vin john' src='img/john1.png'></div>
+				<div class='col-md-7 col-sm-12 col-xs-12'><img class='vin john' src='img/products/".$product["image"]."'></div>
 				<div class='col-md-5 col-sm-12 col-xs-12 vinylText relaa'>
 					<h2>".$product["item"]."<br>
 					<a href=artists.php?artist_id=".$product["artist_id"]."><span class='vinylText'>".$product["name"]."</span></h2></a>
 					<div style='font-size: 17px'>".$product["type"]."</div><br>
-					<h3>".$product["description"]."</h3><br>
+					<h3 id='productDescription".$product["product_id"]."'>".substr($product["description"], 0, 400)."<span class='readMore".$product["product_id"]."'>...&nbsp &nbsp </span><span class='readMore".$product["product_id"]."' style='color:black; font-size:15px; text-decoration:underline'>Read More</span></h3><br>
 					<br>
 						<button class='price price1'>$".$product["price"]."</button>
 				</div>
@@ -74,5 +74,21 @@
 	<script src="js/jquery-3.6.0.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/script.js"></script>
+	<script type="text/javascript">
+
+<?php
+	foreach ($products as $product) {
+		echo "
+
+			let characters".$product["product_id"]." = $('#productDescription".$product["product_id"]."').text().length;
+
+			if(characters".$product["product_id"]." < 414){
+				$('.readMore".$product["product_id"]."').css('display', 'none');
+			}
+
+		";
+	}
+?>
+	</script>
 	</body>
 </html>
